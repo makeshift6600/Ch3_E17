bdf9068d68a1c2142d1d7073454de7f18938f9cc
makeshift6600 <j.l.cannady04@gmail.com>
2025-09-24 09:19:23 -0400

    uses rand

--- Ch3_E17.cpp
+++ Ch3_E17.cpp
@@ -11,14 +11,14 @@ Requirments: present two random numbers and then pause before displaying the tot
 using namespace std;
 
 int main() {
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<int> dist(1, 1000);
-    int Num1 = dist(gen);
-    int Num2 = dist(gen);
+    srand(time(0));
+    int Num1 = rand() % 10001;
+    int Num2 = rand() % 10001;
+   
     int Total = (Num1 + Num2);
     cout << "Add " << Num1 << " and " << Num2 << endl;
     cout << "Press enter to see the answer";
     cin.get();
 
     cout << Total;
+}
